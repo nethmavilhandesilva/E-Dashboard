ad600ea6bda9d657068a81d6eb608fd9dcc70c32
Nethma de silva <nethmavilhan@gmail.com>
2025-02-10 16:03:51 +0530

    Add all changes

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require("express");
 require("./db/config");
+const User = require('./db/config');
 const app = express();
-const User = require('./db/User');
 
-app.use(express.json());
-app.post("/register",async(req, resp)=>{
-    let user = new User(req.body);
-    let result = await user.save();
-    resp.send(result)
+app.post("/register",(req,resp)=>{
+    resp.send("api in progress")
 })
 
 app.listen(5000);
