2a6169c495f135a0e48504c81cabff984843cc72
Nethma de silva <nethmavilhan@gmail.com>
2025-02-12 17:54:12 +0530

    Add all changes

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import Nav from './components/Nav';
 import React from 'react';
 import Footer from './components/Footer';
 import SignUp from './components/SignUp';
-
+import PrivateComponent from './components/PrivateComponent';
+import Login from './components/Login';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Nav />
         <Routes>
+          <Route element={<PrivateComponent/>}> 
           <Route path="/" element={<h1>Product Component</h1>} />
           <Route path="/add" element={<h1>Add Product Component</h1>} />
           <Route path="/update" element={<h1>Update Product Component</h1>} />
           <Route path="/logout" element={<h1>Logout Component</h1>} />
           <Route path="/profile" element={<h1>Profile Component</h1>} />
+          </Route>
           <Route path="/signup" element={<SignUp/>} />
+          <Route path="/login" element={<Login/>} />
         </Routes>
       </BrowserRouter>
       <Footer />
